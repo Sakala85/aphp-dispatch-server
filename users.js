490f9ac5540511879a263f4a0dee78ff4ddb50c3
melchior <melchiorbengtsson@gmail.com>
2020-04-16 13:58:53 +0200

    ok

--- users.js
+++ users.js
@@ -165,7 +165,7 @@ const getTask = () => {
 const getUser = () => {
   const userSorted = users.filter((user) => user.online === 1);
   userSorted.concat(users.filter((user) => user.online === 2));
-  // userSorted.push(users.filter((user) => user.online === 0));
+  userSorted.concat(users.filter((user) => user.online === 0));
   return userSorted;
 };
 
